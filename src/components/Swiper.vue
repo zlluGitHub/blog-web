8898137c7edce646cb038b78902f6d4b04575321
zhenglinglu <l>
2020-06-27 15:10:44 +0800

    update

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,9 +1,23 @@
 
 <template>
   <div class="swiperBox">
-    <Carousel autoplay v-model="value2" loop>
-      <CarouselItem v-for="(banner,i) in banners" :key="i">
+    <Carousel autoplay v-model="value2" loop :autoplay-speed="3000">
+      <!-- <CarouselItem v-for="(banner,i) in banners" :key="i">
         <img :src="banner" />
+      </CarouselItem>-->
+      <CarouselItem>
+        <img
+          src="../assets/image/20200627135247.jpg"
+          alt="前端自动化部署平台"
+          @click.stop="handleRouter('http://swd.zhenglinglu.cn/')"
+        />
+      </CarouselItem>
+      <CarouselItem>
+        <img
+          src="../assets/image/20200627141721.jpg"
+          alt="前端自动化部署平台"
+          @click.stop="handleRouter('https://github.com/zlluGitHub/web-deploy')"
+        />
       </CarouselItem>
     </Carousel>
   </div>
@@ -22,7 +36,14 @@ export default {
       ]
     };
   },
-  methods: {}
+  methods: {
+    handleRouter(href) {
+      let win = window.open();
+      win.opener = null;
+      win.location = href;
+      win.target = "_blank";
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -30,6 +51,8 @@ export default {
   img {
     width: 100%;
     height: 390px;
+    display: block;
+    cursor: pointer;
   }
 }
 </style>
